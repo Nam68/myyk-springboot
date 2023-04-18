d5fe8e2896d8d912c4cee667ee104bb5a903e245
Nam <epoche02@naver.com>
2023-04-18 20:20:59 +0900

    홈페이지 개수 중

--- myyk/src/main/java/yk/web/myyk/backend/controller/global/GlobalController.java
+++ myyk/src/main/java/yk/web/myyk/backend/controller/global/GlobalController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import yk.web.myyk.backend.controller.BaseController;
 import yk.web.myyk.backend.entity.member.MemberEntity;
 import yk.web.myyk.backend.repository.MemberRepository;
@@ -17,9 +18,9 @@ public class GlobalController extends BaseController {
 	MemberRepository m;
 	
 	@RequestMapping({"/", "/global/homepage.do"})
-	public String homepage() {
+	public String homepage(HttpServletRequest request) {
 		
-		List<MemberEntity> list = m.findAll();
+		request.setAttribute("category", "MEMBER");
 		
 		return "global/homepage";
 	}
