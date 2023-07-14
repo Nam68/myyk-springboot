2a44f2bc1b112f609f397c8ad068fb7de3d5c62a
epoch <epoch@192.168.2.107>
2023-07-14 22:00:48 +0900

    필요 없는 임포트 삭제

--- myyk/src/main/java/yk/web/myyk/config/WebMvcConfig.java
+++ myyk/src/main/java/yk/web/myyk/config/WebMvcConfig.java
@@ -1,12 +1,8 @@
 package yk.web.myyk.config;
 
-import java.time.Duration;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 import yk.web.myyk.util.interceptor.CategoryInterceptor;
 import yk.web.myyk.util.interceptor.LanguageInterceptor;
