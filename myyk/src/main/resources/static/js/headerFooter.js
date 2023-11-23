f8c8201046bd16770027e2f9f5a0516cf7ea375d
epoch <epoch@192.168.2.129>
2023-11-23 13:13:43 +0900

    헤더 푸터 리부트

--- myyk/src/main/resources/static/js/headerFooter.js
+++ myyk/src/main/resources/static/js/headerFooter.js
@@ -10,9 +10,8 @@
 function clickFooterIcon(item) {
     let url = 'https://';
     switch (item) {
-        case 'twitter': url += 'www.twitter.com/'; break;
-        case 'instagram': url += 'www.instagram.com/'; break;
-        case 'facebook': url += 'facebook.com/'; break;
+        case 'instagram': url += 'www.instagram.com/___im___0916/'; break;
+        case 'facebook': url += 'www.facebook.com/youngkyu.nam'; break;
         case 'steam': url += 'steamcommunity.com/profiles/76561198084622327/'; break;
     }
     window.open(url, '_blank');
